ec90b7b8ac954ca14cfa26d3a21978698177b5e6
antoinerime <antoine.rime@student.uclouvain.be>
2019-10-08 15:31:11 +0200

    Add Hello World plugin

---
+++ plugins/HelloWorld/HelloWorld.c
@@ -0,0 +1,12 @@
+//
+// Created by antoine on 02.10.19.
+//
+#include <stdio.h>
+#include "picotls/picotls_struct.h"
+#include "picotls.h"
+
+int hello_word()
+{
+    help_printf_str("Hello World");
+    return 0;
+}
